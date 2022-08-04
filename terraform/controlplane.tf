f0b82efd641f722612df6ba755bda6a80d8c97af
blankdots <stefan@blankdots.com>
2022-08-04 13:31:43 +0300

    add audit policy for logging

--- terraform/controlplane.tf
+++ terraform/controlplane.tf
@@ -30,6 +30,7 @@ resource "libvirt_cloudinit_disk" "server-init" {
       KUBERNETES_JOIN_TOKEN           = var.kubernetes_join_token,
       KUBERNETES_NODES_IPS            = join(",", var.kubernetes_worker_ips),
       KUBERNETES_MASTER_IPS           = join(",", setsubtract(var.kubernetes_server_ips, [element(var.kubernetes_server_ips, count.index)])),
+      KUBERNETES_AUDIT_POLICY         = file("${path.cwd}/presets/audit_policy.yaml"),
   })
 
 }
